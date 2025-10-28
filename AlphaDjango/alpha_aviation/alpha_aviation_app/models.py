971ba3696449814fcaecd129dd555789eefd20e7
RhettTheRhett <93895330+RhettTheRhett@users.noreply.github.com>
2025-10-27 19:18:59 -0600

    templates, Aircraft model, views, url
    html templates to be able to visualize data
    aircraft model to have a data object
    views for html to recieve requests
    url page updated

--- AlphaDjango/alpha_aviation/alpha_aviation_app/models.py
+++ AlphaDjango/alpha_aviation/alpha_aviation_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Aircraft(models.Model):
+    registration_number = models.IntegerField(max_length=200)
+    model = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    year_built = models.IntegerField(validators=[MaxValueValidator(9999),MinValueValidator(1903)])
+    def __str__(self):
+        return self.objects.all()
+
--- AlphaDjango/alpha_aviation/alpha_aviation_app/views.py
+++ AlphaDjango/alpha_aviation/alpha_aviation_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Aircraft
+
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def aircraft_specs(request):
+    airplanes = Aircraft.objects.all()
+    context = {"airplanes": airplanes}
+    return render(request, "aircraft_specs.html", context)
