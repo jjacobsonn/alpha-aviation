80745338fffbabbb122166185b0bf282771612fe
RhettTheRhett <rhettpalmer1@gmail.com>
2025-11-01 13:26:15 -0600

    Rest API integration
    Added serializers and changed the view sets to support rest api stuff. added second temporary model "part"

--- AlphaDjango/alpha_aviation/alpha_aviation_app/models.py
+++ AlphaDjango/alpha_aviation/alpha_aviation_app/models.py
@@ -7,7 +7,16 @@ class Aircraft(models.Model):
     registration_number = models.IntegerField(max_length=200)
     model = models.CharField(max_length=200)
     manufacturer = models.CharField(max_length=200)
+    engine_type = models.CharField(max_length=200, null= True)
     year_built = models.IntegerField(validators=[MaxValueValidator(9999),MinValueValidator(1903)])
+    
     def __str__(self):
         return f"{self.registration_number} ({self.model})"
-
+    
+class Part(models.Model):
+    part_number = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    aircraft = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
+    in_stock = models.PositiveIntegerField(default=0)
+    last_inspected = models.DateField(null= True)
--- AlphaDjango/alpha_aviation/alpha_aviation_app/views.py
+++ AlphaDjango/alpha_aviation/alpha_aviation_app/views.py
@@ -1,8 +1,13 @@
+from rest_framework import viewsets
+from .models import Aircraft, Part
+from .serializers import AircraftSerializer, PartSerializer
+
+# Create your views here.
+"""
+old test views
 from django.shortcuts import render, HttpResponse
 from .models import Aircraft
 
-
-# Create your views here.
 def home(request):
     return render(request, "home.html")
 
@@ -11,3 +16,12 @@ def aircraft_specs(request):
     airplanes = Aircraft.objects.all()
     context = {"airplanes": airplanes}
     return render(request, "aircraft_specs.html", context)
+"""
+
+class AircraftViewSet(viewsets.ModelViewSet):
+    queryset = Aircraft.objects.all()
+    serializer_class = AircraftSerializer
+
+class PartViewSet(viewsets.ModelViewSet):
+    queryset = Part.objects.all()
+    serializer_class = PartSerializer
