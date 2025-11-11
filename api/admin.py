f2676d79fd908f2d00a8ecbfa7c775e64aaee2b3
Kyle-Bluemel <kyle@bluemel.me>
2025-11-11 16:07:22 -0700

    Tweaked user model

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Profile, Company, Aircraft, Part
 # Register your models here.
 
-admin.site.register(Profile)
-admin.site.register(Company)
+class AircraftInline(admin.TabularInline):
+    model = Aircraft
+    extra = 1
+
+class ProfileInline(admin.TabularInline):
+      model = Profile
+      fields  =('company', 'companyRole', 'username', 'first_name', 'last_name') 
+      extra = 1
+      show_change_link = True
+
+class CompanyAdmin(admin.ModelAdmin):
+        inlines = [AircraftInline, ProfileInline]
+
+class CustomUserAdmin(UserAdmin):
+      fieldsets = UserAdmin.fieldsets + (
+            ('Additional Info', {'fields':('company', 'companyRole')}),
+      )
+      add_fieldsets = UserAdmin.add_fieldsets +(
+            ('Additional Info', {'fields':('company', 'companyRole')}),
+      )
+
+admin.site.register(Profile, CustomUserAdmin)
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(Aircraft)
 admin.site.register(Part)
--- api/models.py
+++ api/models.py
@@ -7,8 +7,18 @@ class Company(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 class Profile(AbstractUser):
+  roleChoices = [
+      ('owner', 'Owner'),
+      ('mechanic', 'Mechanic'),
+      ('pilot', 'Pilot'),
+  ]
   company = models.ForeignKey(Company, on_delete=models.SET_NULL, related_name="Users", null=True, blank=True)
+  companyRole = models.CharField(max_length= 255, choices=roleChoices, default='pilot' )
+  
 
 
 
